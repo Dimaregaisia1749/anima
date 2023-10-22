877e5c040516e350a44f0e840648bf78f033f50d
FannDrann <124501545+FannDrann@users.noreply.github.com>
2023-10-22 13:29:11 +0300

    hero_run_sprite

--- scripts/PlayerController.gd
+++ scripts/PlayerController.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @export var speed = 300
 @export var bullet: PackedScene
+@onready var animation = $AnimatedSprite2D
 var can_choot = true
 var bullet_speed = 20
 var shoot_delay = 0.2
@@ -24,6 +25,15 @@ func _physics_process(delta):
 	var input_direction  = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction  * speed * delta * 100
 	move_and_slide()
+	
+	if Input.is_action_pressed("right") or Input.is_action_pressed("up") or Input.is_action_pressed("down"):
+		animation.scale.x = 1
+		animation.play("run")
+	elif Input.is_action_pressed("left"):
+		animation.scale.x = -1
+		animation.play("run")
+	else:
+		animation.play("idle")
 	
 func shoot():
 	var bullet_instance = bullet.instantiate()
