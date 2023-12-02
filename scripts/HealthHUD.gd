a52cc172c97bf511e4691ae012e95219fb8e06bd
Dimaregaisia1749 <dmeln1749@gmail.com>
2023-12-02 22:35:12 +0300

    add coins and hearts

--- scripts/HealthHUD.gd
+++ scripts/HealthHUD.gd
@@ -9,10 +9,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$ScoreText.text = 'score: ' + str(singletone_node.score)
+	get_node("ItemList2/MarginContainer/VBoxContainer/Label").text = 'score: ' + str(singletone_node.score)
 	if health > player.health:
 		health = player.health
 		var heart_node = get_node("ItemList/MarginContainer/HBoxContainer/Heart" + str(health + 1))
 		var tween = create_tween()
 		tween.tween_property(heart_node, "rotation_degrees", 90, 0.5).set_trans(Tween.TRANS_BACK)
-		tween.tween_property(heart_node, "position", heart_node.position + Vector2.DOWN * 200, 1).set_trans(Tween.TRANS_BACK)
+		tween.tween_property(heart_node, "position", heart_node.position + Vector2.DOWN * 25, 1).set_trans(Tween.TRANS_EXPO)
+		tween.tween_property(heart_node, "modulate:a", 0, 0.2).set_trans(Tween.TRANS_EXPO)
